5475a7ba9cdb8bc80a24b35036300ef32bec8ff5
tehya-debug <97810690+tehya-debug@users.noreply.github.com>
2022-07-08 01:44:49 -0700

    add sound

--- checkbox.lua
+++ checkbox.lua
@@ -2,13 +2,23 @@
 -- https://github.com/Facepunch/garrysmod/blob/4aefc7d7f468998cd3239c0baa560a389f27d884/garrysmod/lua/vgui/dcheckbox.lua#L6
 
 function DCheckBox:Paint(w, h)
-    local boxMaterial = Material("materials/icon16/cross.png", "nomips")
-    
-    if (self:GetChecked()) then
-        boxMaterial = Material("materials/icon16/accept.png", "nomips")
-    end
+	local boxMaterial = Material("materials/icon16/cross.png", "nomips")
+	
+	if (self:GetChecked()) then
+		boxMaterial = Material("materials/icon16/accept.png", "nomips")
+	end
 
-    surface.SetMaterial(boxMaterial)
+	surface.SetMaterial(boxMaterial)
 	surface.SetDrawColor(255, 255, 255)
 	surface.DrawTexturedRect(0, 0, w, h)
+end
+
+function DCheckBox:Toggle()
+	if (not self:GetChecked()) then
+		self:SetValue(true)
+	else
+		self:SetValue(false)
+	end
+
+	surface.PlaySound("buttons/button14.wav")
 end
